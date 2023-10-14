83550858a5b36177cee12167e40fe2688374cf57
Varela87 <mev_tec@yahoo.com.ar>
2023-10-14 18:20:03 -0300

    genero archivos de proveedores

--- views/spareparts.php
+++ views/spareparts.php
@@ -314,7 +314,8 @@ $categoria_form = new AutomaticForm(
                     <div class="space-y-4">
                         <div>
                             <label for="repuesto" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repuesto</label>
-                            <input type="text" name="repuesto" id="editar-repuesto" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="repuesto" placeholder="repuesto" required="">
+                            <input type="text" name="repuesto" id="editar-repuesto" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"  value="repuesto_nombre" placeholder="repuesto" required="">
+                            
                         </div>
                         <div>
                             <label for="descripcion" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripcion</label>
